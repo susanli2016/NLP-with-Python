438ee898b82c4b063c6cb9358d608efefcebefa2
Susan Li <susanli2005@hotmail.com>
2018-06-17 22:28:31 -0400

    Add file

---
+++ helper.py
@@ -0,0 +1,12 @@
+import os
+
+
+def load_data(path):
+    """
+    Load dataset
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    return data.split('\n')
